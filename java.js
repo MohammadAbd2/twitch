de0f14f40756df51110a3deea5884052bf867d1e
Mohammad Abd <mohammad.abd.dk@gmail.com>
2017-07-27 20:11:12 +0200

    tv switch working..

--- java.js
+++ java.js
@@ -1,7 +1,15 @@
 $( document ).ready(function() {
-$.get("https://api.twitch.tv/kraken/channels/mohammadabddk?client_id=jq4t5b9sqhomu072x28k048brus9ra&name=ttv-165166566", function(chanele){
-  console.log(chanele);
-  $("#container").html($("#container").html() + "<div class='users'><div class='title'>" + chanele.display_name + "<div><div class=''" );
-
+$.getJSON( "https://api.twitch.tv/kraken/channels/mohammadabddk?client_id=jq4t5b9sqhomu072x28k048brus9ra&name=ttv-165166566", function (data) {
+console.log(data);
+    function image(){
+     if(data.logo == null){
+        return data.logo = "/img/profile-image.png";
+      };
+    };
+   image();
+   //data[1].forEach(function (data2){
+     $("#container").html($("#container").html() + "<div class='users'><div class='title'>"
+      + data.display_name + "   - language: "+ data.language + "</div><img class='profile-image' src='" + data.logo + "'></div>" );
+   //});
 });
 });
